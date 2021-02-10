de7f61a4dc1df63ed4cf120640313825fbb549df
Alejandro Caicedo <44643459+AlejoJr@users.noreply.github.com>
2021-02-10 22:13:11 +0000

    Update testR.r

--- testR.r
+++ testR.r
@@ -1,16 +1,9 @@
-llamadas_cluster$COD_LOCALIDAD = NULL
-llamadas_cluster$POBLACION = NULL
-llamadas_cluster$RATIO = NULL
-llamadas_cluster$CANT_LLAMADAS = NULL
+#colocamos una semilla
+set.seed(123)
+#ahora bien dividiremos la data y agruparemos en 6 grupos
+km.res <- kmeans(llamadas_cluster, 6, nstart = 50)
 
 
-#eliminamos las ciudades donde no se han registrado llamadas
-llamadas_cluster = llamadas_cluster[-c(20), ]
-
-
-#eliminamos valores
-llamadas_cluster$nombre= NULL
-llamadas_cluster$cluster= NULL
-
-#redondeamos los valores a dos decimales
-llamadas_cluster[,-1] <-round(llamadas_cluster[,-1],2)
+#cluster obtenido
+print(km.res)
+head(km.res$cluster, 4)

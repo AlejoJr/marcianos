35fce5710995809d9377d2a29bf37361c1c711c1
Alejandro Caicedo <44643459+AlejoJr@users.noreply.github.com>
2021-02-10 21:25:58 +0000

    Update testR.r

--- testR.r
+++ testR.r
@@ -1,5 +1,5 @@
-km.res <- kmeans(llamadas_cluster, 5, nstart = 50)
-
-#cluster obtenido
-print(km.res)
-head(km.res$cluster, 4)
+datos_del_2018 = summarise(group_by(data_set_bogota[data_set_bogota$ANIO == 2018,],
+                                        COD_LOCALIDAD,
+                                        TIPO_INCIDENTE
+                                    ),
+                               cant_llamadas = sum(CANT_INCIDENTES))

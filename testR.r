b2b24a1ab889835d2c832acf16419d7520c130b9
Alejandro Caicedo <44643459+AlejoJr@users.noreply.github.com>
2021-02-10 22:21:52 +0000

    Update testR.r

--- testR.r
+++ testR.r
@@ -1,6 +1,14 @@
-km.res <- kmeans(llamadas_cluster, 3, nstart = 50)
+km.res <- kmeans(llamadas_cluster, 5, nstart = 50)
 
+#cluster obtenido
+print(km.res)
+head(km.res$cluster, 4)
+
+#tamaño
 km.res$size
+
+#medianas 
+km.res$centers
 
 #Visualizando la data obtenida
 fviz_cluster(km.res, data = llamadas_cluster)

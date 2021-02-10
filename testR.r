2cdf25420b0c301f005bdda20142c956a05bf56f
Alejandro Caicedo <44643459+AlejoJr@users.noreply.github.com>
2021-02-10 22:14:58 +0000

    Update testR.r

--- testR.r
+++ testR.r
@@ -7,3 +7,6 @@ km.res <- kmeans(llamadas_cluster, 6, nstart = 50)
 #cluster obtenido
 print(km.res)
 head(km.res$cluster, 4)
+
+#Visualizando la data obtenida
+fviz_cluster(km.res, data = llamadas_cluster)

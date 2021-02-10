ca540b2bb2063d88ddbfe452bcc599d02a51ba02
Alejandro Caicedo <44643459+AlejoJr@users.noreply.github.com>
2021-02-10 22:19:18 +0000

    Update testR.r

--- testR.r
+++ testR.r
@@ -1,12 +1,6 @@
-#colocamos una semilla
-set.seed(123)
-#ahora bien dividiremos la data y agruparemos en 6 grupos
-km.res <- kmeans(llamadas_cluster, 6, nstart = 50)
+km.res <- kmeans(llamadas_cluster, 3, nstart = 50)
 
-
-#cluster obtenido
-print(km.res)
-head(km.res$cluster, 4)
+km.res$size
 
 #Visualizando la data obtenida
 fviz_cluster(km.res, data = llamadas_cluster)

be546d5e295916799794057e1a233ab19daa59ac
Alejandro Caicedo <44643459+AlejoJr@users.noreply.github.com>
2021-02-10 19:23:05 +0000

    Update testR.r

--- testR.r
+++ testR.r
@@ -1,3 +1,9 @@
-delitos_ABC = delitos_clasificacion[delitos_clasificacion$CLASIFICACION == 'A' | 
-                                     delitos_clasificacion$CLASIFICACION == 'B' |
-                                     delitos_clasificacion$CLASIFICACION == 'C',]
+
+llamadas_cluster$COD_LOCALIDAD = NULL
+llamadas_cluster$POBLACION = NULL
+llamadas_cluster$RATIO = NULL
+llamadas_cluster$CANT_LLAMADAS = NULL
+
+#Eliminar ciudades donde no registra llamadas
+llamadas_cluster = llamadas_cluster[-c(20), ]
+

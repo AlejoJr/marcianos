b56b7031ed8b1ae363b0a80720303e10f3de6263
Alejandro Caicedo <44643459+AlejoJr@users.noreply.github.com>
2021-02-10 19:36:17 +0000

    Update testR.r

--- testR.r
+++ testR.r
@@ -1,17 +1,8 @@
 
-for (i in 1:nrow(delitos_ABC))
-{
-  #filtramos tipo de accidente
-  c =datos_del_2018[datos_del_2018$TIPO_INCIDENTE==delitos_ABC$TIPO_INCIDENTE[i],]
-  #creamos el nombre de la columna
-  colName <- paste("D_",delitos_ABC$TIPO_INCIDENTE[i], sep="")
-  
-  
-  #agregamos la cantidad de llamadas anuales por localidad por cada 10000
-  llamadas_localidad[colName] = c$cant_llamadas[match( llamadas_localidad$COD_LOCALIDAD, 
-                                                       c$COD_LOCALIDAD)]*10000/llamadas_localidad$POBLACION
-  
-  llamadas_localidad[colName] <- replace( llamadas_localidad[colName],is.na( llamadas_localidad[colName]),0)
-  
-}
+llamadas_cluster$D_950= NULL
+llamadas_cluster$D_982= NULL
+llamadas_cluster$D_902= NULL
 
+llamadas_cluster[,-1] <-round(llamadas_cluster[,-1],2)
+
+set.seed(123)

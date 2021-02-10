5c234be754559fd4171ab11b31001885d31edece
Alejandro Caicedo <44643459+AlejoJr@users.noreply.github.com>
2021-02-10 15:54:50 +0000

    Update testR.r

--- testR.r
+++ testR.r
@@ -1,12 +1,33 @@
-data_set_bogota = read.csv(file.choose(),header=TRUE, sep=';')
 
-table(data_set_bogota$ANIO)
-table(data_set_bogota$TIPO_DETALLE)
-sum(data_set_bogota$CANT_INCIDENTES)
+localidad_anio_summarise <- localidad_anio_summarise %>% 
+  mutate(RATIO= round(num_delitos/1000,0))
 
-colnames(data_set_bogota)
+localidad_anio_summarise$NOMBRE = localities$nombre[match( localities$COD_LOCALIDAD, 
+                                                           localidad_anio_summarise$COD_LOCALIDAD)]
 
-data_set_bogota = data_set_bogota[data_set_bogota$TIPO_DETALLE != '-',]
-data_set_bogota = data_set_bogota[data_set_bogota$LOCALIDAD != '-',]
 
-data_set_bogota = data_set_bogota[data_set_bogota$COD_LOCALIDAD != '99',]
+
+summary(localidad_anio_summarise$RATIO)
+
+dm<-localidad_anio_summarise[which(localidad_anio_summarise$COD_LOCALIDAD=="01"), 
+                             c("ANIO","RATIO")]
+plot(dm, lty=1, type="l", ylab="Delitos por distrito", xlab="aÃ±o", ylim=c(1,700), 
+     col=1, xlim=c(2015,2020),
+     main="Cantidad de llamadas por distrito")
+
+
+
+for (i in 1:nrow(localities))
+{
+  if(i!=1){
+    dm<-localidad_anio_summarise[which(
+      localidad_anio_summarise$COD_LOCALIDAD==localities$COD_LOCALIDAD[i]), 
+                                 c("ANIO","RATIO")]
+    lines(dm,lty=2, col=i, xlim=c(2015,2020),
+          main="Delitos")
+  }
+  
+}
+
+legend("topright", localities$nombre,
+       col=c(1:nrow(localities)), lty=1:nrow(localities), cex=0.5)

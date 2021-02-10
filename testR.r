69dc78a26fce95751fba19f447d0e71f79d91494
Alejandro Caicedo <44643459+AlejoJr@users.noreply.github.com>
2021-02-10 19:14:29 +0000

    Update testR.r

--- testR.r
+++ testR.r
@@ -1,4 +1,3 @@
-ggplot(delitos_mas_frecuentes_2019, aes(x = NUM_DELITOS, y=NOMBRE_DELITO, color=TIPO_INCIDENTE)) +
-geom_col() + theme(legend.position = "top") +
-labs(title = "Cantidad de incidentes del 2019", subtitle = "Basado en su tipo", x = "Número de delitos (en miles)", color="Codigo de incidente", y = "Incidentes" ) +
-guides(colour = guide_legend(override.aes = list(size=10))) + theme(legend.key = element_rect(fill = "white"))
+delitos_ABC = delitos_clasificacion[delitos_clasificacion$CLASIFICACION == 'A' | 
+                                     delitos_clasificacion$CLASIFICACION == 'B' |
+                                     delitos_clasificacion$CLASIFICACION == 'C',]
